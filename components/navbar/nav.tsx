2ad5d8ab32d8389b8e67900c1f825ae5dffde353
GeorgienDev <GeorgienDev@users.noreply.github.com>
2025-11-11 23:45:26 +0400

    Initial commit

---
+++ components/navbar/nav.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Nav(){
+    return(
+        <>
+        <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand p-3" href="#">Txt Haven</a>
+        </div>
+        </nav>
+        </>
+    );
+
+}
